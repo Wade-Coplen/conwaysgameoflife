508757f823f3ad8a0e8f3371ab4d9aec03e2e1b6
Wade-Coplen <wadecoplen@gmail.com>
2020-10-26 11:48:22 -0700

    auto run is working

--- game_of_life.js
+++ game_of_life.js
@@ -1,10 +1,17 @@
-//need rows and columns
+//I will need to know when the 'start' button has been clicked
+//and as such, know if what 'state' is currently being toggled. 
+//repeat the evolution process
+//speed control
+let start = false // set tru when click starts
+let timer; //evo control
+let evolutionSpeed = 500; // one second per gen
 
+//need rows and columns
 const ROWS = 40;
 const COLUMNS = 40;
 //double buffer
-let currentGeneration = [rows];
-let nextGeneration = [rows];
+let currentGeneration = [ROWS];
+let nextGeneration = [ROWS];
 
 function createGenerationalArray() {
     for(let i = 0; i < ROWS; i++) {
@@ -40,7 +47,7 @@ for(let i = 0; i < ROWS; i++) {
             cell.setAttribute('id', `${i}${'_'}${j}`)
             cell.setAttribute('class', 'dead')
             //add an event listener for cell onClick
-            cell.addEventListener('click', onClick);
+            cell.addEventListener('click', cellClick);
             //create a table data element and assign it to a cell variable
             tableRow.appendChild(cell);
             //append the child element cell to the table row
@@ -52,7 +59,7 @@ for(let i = 0; i < ROWS; i++) {
     //appending upwards once again. the table will append the gameBoard or world. 
 }
 //onClick function
-function onClick() {
+function cellClick() {
     let location = this.id.split('_');
     let row = Number(location[0]); //grab i
     let col = Number(location[1]); //grab j
@@ -65,6 +72,135 @@ function onClick() {
         currentGeneration[row][col] = 1;
     }
 }
+function makeNextGeneration() {
+    for(row in currentGeneration) {
+        for(col in currentGeneration[row]) {
+            let neighbors = neighborsCount(row, col);
+            //Check the rules
+            //if Alive
+            if(currentGeneration[row][col] == 1) {
+                if (neighbors < 2) {
+                    nextGeneration[row][col] = 0;
+                } else if(neighbors == 2 || neighbors == 3) {
+                    nextGeneration[row][col] = 1;
+                } else if(neighbors > 3) {
+                    nextGeneration[row][col] = 0;
+                }
+            } else if(currentGeneration[row][col] == 0) {
+                //if Dead or Empty
+                if(neighbors == 3) {
+                    //propogate
+                    nextGeneration[row][col] = 1;
+                }
+            }
+        }
+    }
+}
+function neighborsCount(row, col) {
+    let count = 0;
+    let neighborRow = Number(row);
+    let neighborColumn = Number(col);
+        //Make sure we are not in the first row
+        if(neighborRow - 1 >= 0) {
+        //check top neighbor
+        if(currentGeneration[neighborRow - 1][neighborColumn] == 1)
+            count++;
+        }
+        //make sure we are not in the first cell
+        //upper left corner
+        if(neighborRow - 1 >= 0 && neighborColumn - 1 >= 0) {
+        //check upper left neighbor
+        if(currentGeneration[neighborRow - 1][neighborColumn - 1] == 1)
+            count++;
+        }
+        //Make sure we are not on the first row last column
+        if(neighborRow - 1 >= 0 && neighborColumn + 1 < COLUMNS) {
+            if(currentGeneration[neighborRow - 1][neighborColumn + 1] == 1)
+                count++;
+        }
+    //Make sure we are not on the first column
+    if(neighborColumn - 1 >= 0) {
+        //Check left neighbor
+        if(currentGeneration[neighborRow][neighborColumn - 1] == 1)
+            count++;
+    }
+    //Make sure we are not on the last column
+    if(neighborColumn + 1 >= 0) {
+        //Check left neighbor
+        if(currentGeneration[neighborRow][neighborColumn + 1] == 1)
+            count++;
+    }
+    //Make sure we are not on the bottom left corner
+    if(neighborRow + 1 < ROWS && neighborColumn - 1 >= 0) {
+        //Check bottom left neighbor
+        if(currentGeneration[neighborRow + 1][neighborColumn - 1] == 1)
+            count++;
+    }
+    if(neighborRow + 1 < ROWS && neighborColumn + 1 < COLUMNS) {
+        //Check bottom right neighbor
+        if(currentGeneration[neighborRow + 1][neighborColumn + 1] == 1)
+            count++
+    }
+    //Make sure we are not on the last row
+    if(neighborRow + 1 < ROWS) {
+        //Check bottom neighbor
+        if(currentGeneration[neighborRow + 1][neighborColumn] == 1)
+            count++;
+    }
+    return count;
+}
+    function updateCurrentGeneration() {
+        for(row in currentGeneration) {
+            for(col in currentGeneration[row]) {
+                //update the current generation with 
+                //the results of the createNextGeneration function
+                currentGeneration[row][col] = nextGeneration[row][col];
+                //set nextGeneration back to empty
+                nextGeneration[row][col] = 0;
+            }
+        }
+    }
+function updateRunWorld() {
+    let cell = '';
+    for(row in currentGeneration) {
+        for(col in currentGeneration[row]) {
+            cell = document.getElementById(`${row}${'_'}${col}`);
+            if(currentGeneration[row][col] == 0) {
+                cell.setAttribute('class', 'dead');
+            } else {
+                cell.setAttribute('class', 'alive')
+            }
+        }
+    }
+}
+
+function evolution() {
+    makeNextGeneration();
+    updateCurrentGeneration();
+    updateRunWorld();
+
+    if (start) {
+        timer = setTimeout(evolution, evolutionSpeed)
+    }
+}
+function startStop() {
+    //if state is 'start'
+    let startstop = document.querySelector('#buttonstartandstop');
+
+    if(!start) {
+        start = true;
+        startstop.value = 'Stop Production';
+        evolution();
+    } else {
+        start = false;
+        startstop.value = 'Start Production';
+        clearTimeout(timer);
+    }
+}
+function resetWorld() {
+    location.reload();
+}
+
 window.onload=()=>  {
     runWorld(); //the grid
     createGenerationalArray(); //current and next gens
